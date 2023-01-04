4594ed49c8ea91c40521874cae83c4eda00d57be
Anastasiia Klyosova <112250768+LeniFraud@users.noreply.github.com>
2023-01-04 18:04:34 +0200

    update task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,29 +5,9 @@ const iframeRef = document.querySelector('iframe');
 const player = new Player(iframeRef);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-getCurrentTime();
-
-player
-  .setCurrentTime(getCurrentTime())
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0);
 
 player.on('timeupdate', throttle(updateCurrentTime, 1000));
-
-function getCurrentTime() {
-  return Number(localStorage.getItem(LOCALSTORAGE_KEY)) || 0;
-}
 
 function updateCurrentTime(event) {
   localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,12 +12,16 @@ form.addEventListener('input', throttle(onTextInput, 500));
 form.addEventListener('submit', onSubmitForm);
 
 function updateInput() {
-  const localData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  if (!localData) {
-    return;
+  try {
+    const localData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (!localData) {
+      return;
+    }
+    email.value = localData.email;
+    message.value = localData.message;
+  } catch (err) {
+    console.log('Error: ', err);
   }
-  email.value = localData.email;
-  message.value = localData.message;
 }
 
 function onTextInput() {
@@ -40,3 +44,49 @@ function onSubmitForm(event) {
 
   form.reset();
 }
+
+// import throttle from 'lodash.throttle';
+
+// const form = document.querySelector('.feedback-form');
+// const LOCALSTORAGE_KEY = 'feedback-form-state';
+// // const {
+// //   elements: { email, message },
+// // } = form;
+// const inputData = {};
+
+// updateInput();
+
+// form.addEventListener('input', throttle(onTextInput, 500));
+// form.addEventListener('submit', onSubmitForm);
+
+// function updateInput() {
+//   const localData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+//   if (!localData) {
+//     return;
+//   }
+
+//   for (const el in localData) {
+//     form.elements[el].value = localData[el];
+//   }
+// }
+
+// function onTextInput(event) {
+//   const fieldName = event.target.name;
+//   const fieldValue = event.target.value;
+
+//   inputData[fieldName] = fieldValue;
+//   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputData));
+// }
+
+// function onSubmitForm(event) {
+//   event.preventDefault();
+
+//   console.log({
+//     email: email.value,
+//     message: message.value,
+//   });
+
+//   localStorage.removeItem(LOCALSTORAGE_KEY);
+
+//   form.reset();
+// }
